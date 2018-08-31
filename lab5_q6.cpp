4ad57cadb6a2dcd2c0b97082e837ad9dc27ecf0b
1raviprakash <42237336+1raviprakash@users.noreply.github.com>
2018-08-31 19:26:07 +0530

    Add files via upload

---
+++ lab5_q6.cpp
@@ -0,0 +1,21 @@
+//library
+#include<iostream>
+using namespace std;
+//main function
+int main()
+{
+	int a;
+	//Give year
+	cout << "year" << endl;
+	cin >> a;
+	// condition
+	if ((a % 4 )== 0 ){
+	cout << a << " is a leap year" << endl;
+	}
+	else { 
+	cout << a << " is not a leap year" << endl; 
+	}
+return 0;
+}
+
+
