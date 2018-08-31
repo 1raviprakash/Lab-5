aba77512dcc38c9679c2289c4162b4b8a7fdbd9c
1raviprakash <42237336+1raviprakash@users.noreply.github.com>
2018-08-31 12:43:13 +0530

    Add files via upload

---
+++ lab5_q26.cpp
@@ -0,0 +1,18 @@
+//Library 
+#include <iostream>
+using namespace std;
+//Main function
+int main()
+{
+int i,n;
+cout << "give the last number" << endl;
+cin >> n;
+// loop
+i= (-1);
+while(i<n){
+//odd number
+i= i+ 2 ;
+cout << i << endl;
+}
+return 0;
+}
