ccd3f81d6da2f8f8376578e8b3550c2f342ca064
1raviprakash <42237336+1raviprakash@users.noreply.github.com>
2018-08-31 10:22:01 +0530

    Add files via upload

---
+++ lab5_q1.cpp
@@ -0,0 +1,23 @@
+//Library 
+#include <iostream>
+using namespace std;
+//Main function
+int main()
+{
+float A,B;
+// Give the numbers
+cout << " Give the numbers" << endl;
+//store the value in A and B
+cin >> A >> B;
+// Condition
+if (A<B){
+//to do
+cout << "B is greate and it is : "<< B << endl;
+}
+//Else go ahead
+else{
+//to do otherwise
+cout << "A is greater and it is : " << A << endl;
+}
+return 0;
+}
