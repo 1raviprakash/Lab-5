a27b13094001ca6a7dcd0fcbcd036a356a0dee33
1raviprakash <42237336+1raviprakash@users.noreply.github.com>
2018-08-31 11:48:46 +0530

    Add files via upload

---
+++ lab5_q5.cpp
@@ -0,0 +1,20 @@
+//Library 
+#include <iostream>
+using namespace std;
+//Main function
+int main()
+{
+int a;
+// Give the numbers
+cout << " Give the numbers" << endl;
+//store the value in a
+cin >> a ;
+// Condition
+if ((a % 2 )== 0 ) {
+cout << a << "is even " << endl;
+}
+else {
+cout << a << " is odd " << endl;
+}
+return 0;
+}
