f1762034e9dedec0f4136efea7a676d16e29062a
1raviprakash <42237336+1raviprakash@users.noreply.github.com>
2018-08-31 12:19:48 +0530

    Add files via upload

---
+++ lab5_q23.cpp
@@ -0,0 +1,18 @@
+//Library 
+#include <iostream>
+using namespace std;
+//Main function
+int main()
+{
+int i,n;
+cout << "give the last number" << endl;
+cin >> n;
+// loop
+i=0;
+while(n>i){
+// a=a-1
+n--;
+cout << n << endl;
+}
+return 0;
+}
